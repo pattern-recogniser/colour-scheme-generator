209d9a64d2ba04aa07f41998746d093f5abc1f1a
Anjali Karimpil <anjaliskarimpil@gmail.com>
2024-02-11 21:59:02 +0000

    Cleaned up code, refactored to take varying length of colours to display

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const selectedColorEl = document.getElementById("selected-color")
 const getColorBtn = document.getElementById("get-color-btn")
-const resultDiv = document.getElementById("result-div")
-const selectorDiv = document.getElementById("selector-div")
 const selectedPalette = document.getElementById("type")
+const resultDiv = document.getElementById("result-div")
+
+// Light mode functionality
 const modeSelectorEl = document.getElementById("mode-selector")
 const containerEl = document.getElementById("container")
 const darkModeEl = document.getElementById("dark-mode")
 const lightModeEl = document.getElementById("light-mode")
 
-function renderColours(){
+getColours()
+modeSelectorEl.addEventListener("click", switchMode)
+getColorBtn.addEventListener("click", getColours)
+
+function getColours(){
     const selectedColor = selectedColorEl.value.replace("#", "")
     const selectedType = selectedPalette.value
-    const baseUrl = "https://www.thecolorapi.com/scheme"
-    const callbackUrl = `hex=${selectedColor}&mode=${selectedType}&count=5`
 
-    fetch(baseUrl + "?" + callbackUrl)
+    const numColorsToDisplay = resultDiv.getElementsByClassName("color-display").length
+    const baseUrl = "https://www.thecolorapi.com/scheme"
+    const queryUrl = `hex=${selectedColor}&mode=${selectedType}&count=${numColorsToDisplay}`
+
+    fetch(baseUrl + "?" + queryUrl)
         .then(res => res.json())
         .then(data => {
             const colorsToDisplay = data.colors.map(color => color.hex.value)
@@ -22,25 +29,17 @@ function renderColours(){
         })
 }
 
-renderColours()
-getColorBtn.addEventListener("click", renderColours)
-
-
-function getColor(){
-    return 
-}
-
 function renderColorScheme(colors){
+
     const resultDivColors = resultDiv.getElementsByClassName("color-display")
     const resultDivColorsValues = resultDiv.getElementsByClassName("color-hex")
-    console.log(resultDivColorsValues)
+
     for (let i=0; i< resultDivColors.length; i++){
         resultDivColors[i].style.backgroundColor = colors[i]
         resultDivColorsValues[i].innerHTML = `<p>${colors[i]}</p>`
     }
 }
 
-modeSelectorEl.addEventListener("click", switchMode)
 function switchMode(){
     darkModeEl.classList.toggle("hidden")
     lightModeEl.classList.toggle("hidden")
